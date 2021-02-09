c8158d5b8b87a6520d6807a757cf8b22d1d4bcf2
M.Akif HASLAK <77489495+MAkif69@users.noreply.github.com>
2021-02-09 19:59:34 +0300

    Add files via upload

---
+++ GameStoreBackendSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿using GameStoreBackendSystem.Abstract;
+using GameStoreBackendSystem.Adapter;
+using System;
+
+namespace GameStoreBackendSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Customer customer = new Customer {Id=1,FirstName="*",LastName="*",
+            BirthTime=new DateTime(*),NationaltiyId="*"};
+
+            BaseCustomerManager baseCustomerManager = new CustomerManager(new CustomerAdapterService());
+            baseCustomerManager.Add((Customer)customer);
+            
+        }
+    }
+}
